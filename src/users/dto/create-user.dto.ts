8eef3108eaad244201cd1de35fb1c8a116b9f014
bquochuy1514 <23520594@gm.uit.edu.vn>
2025-01-17 05:20:38 +0700

    Add create a user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from "class-validator"
+import e from "express"
+import { UserRole } from "../entities/user.entity"
+
+export class CreateUserDto {
+    @IsEmail({}, { message: 'Invalid email format' })
+    @IsNotEmpty({ message: 'Email should not be empty' })
+    email: string
+
+    @IsNotEmpty()
+    password: string
+    
+    name: string
+    phone_number: string
+    role: UserRole
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      // Create new user instance
+      const user = new User();
+      user.email = createUserDto.email;
+      user.password = createUserDto.password;
+      user.name = createUserDto.name;
+      user.phone_number = createUserDto.phone_number;
+      user.role = createUserDto.role;
+      
+      // Save to database
+      await user.save();
+
+      console.log('>>> check user: ', user);
+      
+      // Return created user
+      return user;
+    } catch (error) {
+      throw new Error('Could not create user: ' + error.message);
+    }
   }
 
   findAll() {
