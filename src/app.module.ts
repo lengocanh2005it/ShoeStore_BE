d682d045d2b74c53b76ac3b08f19614aa30fed80
bquochuy1514 <23520594@gm.uit.edu.vn>
2025-01-17 03:10:49 +0700

    add users and products modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { DataSource } from 'typeorm';
+import { Product } from './products/entities/product.entity';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost', 
+      port: parseInt(process.env.MYSQL_PORT) || 3309,
+      username: 'root',
+      password: process.env.MYSQL_ROOT_PASSWORD || 'root',
+      database: process.env.MYSQL_DATABASE || 'shoes-db',
+      entities: [Product, User],
+      synchronize: true, 
+    }),
+    ProductsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+    // Kiểm tra kết nối
+    if(this.dataSource.isInitialized) {
+      console.log('Database connected successfully');
+    }
+    
+    // Thực hiện raw query
+    // this.dataSource.query('SELECT * FROM products')
+    //   .then(products => console.log('>>> check products: ', products))
+  } 
+}
